c4b0b6e93ad06f23b2834682d7947c54593808e8
Michael Käufl <big-fat-rss@c.michael-kaeufl.de>
2014-11-18 23:16:56 +0100

    Fixed phpcs' complaints about multi-line statements

--- include/ccache.php
+++ include/ccache.php
@@ -8,11 +8,15 @@
 
 function ccache_zero_all($owner_uid)
 {
-    db_query("UPDATE ttrss_counters_cache SET
-        value = 0 WHERE owner_uid = '$owner_uid'");
+    db_query(
+        "UPDATE ttrss_counters_cache SET
+        value = 0 WHERE owner_uid = '$owner_uid'"
+    );
 
-    db_query("UPDATE ttrss_cat_counters_cache SET
-        value = 0 WHERE owner_uid = '$owner_uid'");
+    db_query(
+        "UPDATE ttrss_cat_counters_cache SET
+        value = 0 WHERE owner_uid = '$owner_uid'"
+    );
 }
 
 function ccache_remove($feed_id, $owner_uid, $is_cat = false)
@@ -23,17 +27,20 @@ function ccache_remove($feed_id, $owner_uid, $is_cat = false)
         $table = "ttrss_cat_counters_cache";
     }
 
-    db_query("DELETE FROM $table WHERE
-        feed_id = '$feed_id' AND owner_uid = '$owner_uid'");
-
+    db_query(
+        "DELETE FROM $table WHERE
+        feed_id = '$feed_id' AND owner_uid = '$owner_uid'"
+    );
 }
 
 function ccache_update_all($owner_uid)
 {
     if (get_pref('ENABLE_FEED_CATS', $owner_uid)) {
 
-        $result = db_query("SELECT feed_id FROM ttrss_cat_counters_cache
-            WHERE feed_id > 0 AND owner_uid = '$owner_uid'");
+        $result = db_query(
+            "SELECT feed_id FROM ttrss_cat_counters_cache
+            WHERE feed_id > 0 AND owner_uid = '$owner_uid'"
+        );
 
         while ($line = db_fetch_assoc($result)) {
             ccache_update($line["feed_id"], $owner_uid, true);
@@ -44,20 +51,20 @@ function ccache_update_all($owner_uid)
         ccache_update(0, $owner_uid, true);
 
     } else {
-        $result = db_query("SELECT feed_id FROM ttrss_counters_cache
-            WHERE feed_id > 0 AND owner_uid = '$owner_uid'");
+        $result = db_query(
+            "SELECT feed_id FROM ttrss_counters_cache
+            WHERE feed_id > 0 AND owner_uid = '$owner_uid'"
+        );
 
         while ($line = db_fetch_assoc($result)) {
             print ccache_update($line["feed_id"], $owner_uid);
-
         }
 
     }
 }
 
-function ccache_find($feed_id, $owner_uid, $is_cat = false,
-    $no_update = false) {
-
+function ccache_find($feed_id, $owner_uid, $is_cat = false, $no_update = false)
+{
     if (!is_numeric($feed_id)) return;
 
     if (!$is_cat) {
@@ -77,9 +84,11 @@ function ccache_find($feed_id, $owner_uid, $is_cat = false,
         $date_qpart = "updated > DATE_SUB(NOW(), INTERVAL 15 MINUTE)";
     }
 
-    $result = db_query("SELECT value FROM $table
+    $result = db_query(
+        "SELECT value FROM $table
         WHERE owner_uid = '$owner_uid' AND feed_id = '$feed_id'
-        LIMIT 1");
+        LIMIT 1"
+    );
 
     if (db_num_rows($result) == 1) {
         return db_fetch_result($result, 0, "value");
@@ -93,9 +102,13 @@ function ccache_find($feed_id, $owner_uid, $is_cat = false,
 
 }
 
-function ccache_update($feed_id, $owner_uid, $is_cat = false,
-    $update_pcat = true, $pcat_fast = false) {
-
+function ccache_update(
+    $feed_id,
+    $owner_uid,
+    $is_cat = false,
+    $update_pcat = true,
+    $pcat_fast = false
+) {
     if (!is_numeric($feed_id)) return;
 
     /* if (!$is_cat && $feed_id > 0) {
@@ -130,18 +143,22 @@ function ccache_update($feed_id, $owner_uid, $is_cat = false,
         /* Recalculate counters for child feeds */
 
         if (!$pcat_fast) {
-            $result = db_query("SELECT id FROM ttrss_feeds
-                    WHERE owner_uid = '$owner_uid' AND $cat_qpart");
+            $result = db_query(
+                "SELECT id FROM ttrss_feeds
+                WHERE owner_uid = '$owner_uid' AND $cat_qpart"
+            );
 
             while ($line = db_fetch_assoc($result)) {
                 ccache_update($line["id"], $owner_uid, false, false);
             }
         }
 
-        $result = db_query("SELECT SUM(value) AS sv
+        $result = db_query(
+            "SELECT SUM(value) AS sv
             FROM ttrss_counters_cache, ttrss_feeds
             WHERE id = feed_id AND $cat_qpart AND
-            ttrss_feeds.owner_uid = '$owner_uid'");
+            ttrss_feeds.owner_uid = '$owner_uid'"
+        );
 
         $unread = (int) db_fetch_result($result, 0, "sv");
 
@@ -151,19 +168,25 @@ function ccache_update($feed_id, $owner_uid, $is_cat = false,
 
     db_query("BEGIN");
 
-    $result = db_query("SELECT feed_id FROM $table
-        WHERE owner_uid = '$owner_uid' AND feed_id = '$feed_id' LIMIT 1");
+    $result = db_query(
+        "SELECT feed_id FROM $table
+        WHERE owner_uid = '$owner_uid' AND feed_id = '$feed_id' LIMIT 1"
+    );
 
     if (db_num_rows($result) == 1) {
-        db_query("UPDATE $table SET
+        db_query(
+            "UPDATE $table SET
             value = '$unread', updated = NOW() WHERE
-            feed_id = '$feed_id' AND owner_uid = '$owner_uid'");
+            feed_id = '$feed_id' AND owner_uid = '$owner_uid'"
+        );
 
     } else {
-        db_query("INSERT INTO $table
+        db_query(
+            "INSERT INTO $table
             (feed_id, value, owner_uid, updated)
             VALUES
-            ($feed_id, $unread, $owner_uid, NOW())");
+            ($feed_id, $unread, $owner_uid, NOW())"
+        );
     }
 
     db_query("COMMIT");
@@ -176,8 +199,10 @@ function ccache_update($feed_id, $owner_uid, $is_cat = false,
 
             if ($update_pcat) {
 
-                $result = db_query("SELECT cat_id FROM ttrss_feeds
-                    WHERE owner_uid = '$owner_uid' AND id = '$feed_id'");
+                $result = db_query(
+                    "SELECT cat_id FROM ttrss_feeds
+                    WHERE owner_uid = '$owner_uid' AND id = '$feed_id'"
+                );
 
                 $cat_id = (int) db_fetch_result($result, 0, "cat_id");
 
@@ -192,34 +217,41 @@ function ccache_update($feed_id, $owner_uid, $is_cat = false,
     return $unread;
 }
 
-/* function ccache_cleanup($owner_uid)
+/*function ccache_cleanup($owner_uid)
 {
     if (DB_TYPE == "pgsql") {
-        db_query("DELETE FROM ttrss_counters_cache AS c1 WHERE
+        db_query(
+            "DELETE FROM ttrss_counters_cache AS c1 WHERE
             (SELECT count(*) FROM ttrss_counters_cache AS c2
                 WHERE c1.feed_id = c2.feed_id AND c2.owner_uid = c1.owner_uid) > 1
-                AND owner_uid = '$owner_uid'");
+            AND owner_uid = '$owner_uid'"
+        );
 
-        db_query("DELETE FROM ttrss_cat_counters_cache AS c1 WHERE
+        db_query(
+            "DELETE FROM ttrss_cat_counters_cache AS c1 WHERE
             (SELECT count(*) FROM ttrss_cat_counters_cache AS c2
                 WHERE c1.feed_id = c2.feed_id AND c2.owner_uid = c1.owner_uid) > 1
-                AND owner_uid = '$owner_uid'");
+            AND owner_uid = '$owner_uid'"
+        );
     } else {
-        db_query("DELETE c1 FROM
+        db_query(
+            "DELETE c1 FROM
                 ttrss_counters_cache AS c1,
                 ttrss_counters_cache AS c2
             WHERE
                 c1.owner_uid = '$owner_uid' AND
                 c1.owner_uid = c2.owner_uid AND
-                c1.feed_id = c2.feed_id");
+                c1.feed_id = c2.feed_id"
+        );
 
-        db_query("DELETE c1 FROM
+        db_query(
+            "DELETE c1 FROM
                 ttrss_cat_counters_cache AS c1,
                 ttrss_cat_counters_cache AS c2
             WHERE
                 c1.owner_uid = '$owner_uid' AND
                 c1.owner_uid = c2.owner_uid AND
-                c1.feed_id = c2.feed_id");
-
+                c1.feed_id = c2.feed_id"
+        );
     }
 } */
