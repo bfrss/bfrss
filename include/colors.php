cdecf41ae2e8790f74dc66a5bfe9eca78022640c
Michael Käufl <big-fat-rss@c.michael-kaeufl.de>
2014-11-19 00:21:45 +0100

    Fixed issues with if-elseif-else and for

--- include/colors.php
+++ include/colors.php
@@ -155,10 +155,10 @@ function _resolve_htmlcolor($color) {
 
     $color = strtolower($color);
 
-    if (isset($htmlcolors[$color]))
+    if (isset($htmlcolors[$color])) {
         return $htmlcolors[$color];
-    else
-        return $color;
+    }
+    return $color;
 }
 
 ### RGB >> HSL
@@ -171,9 +171,15 @@ function _color_rgb2hsl($rgb) {
     }
     $h = 0;
     if ($delta > 0) {
-        if ($max == $r && $max != $g) $h += ($g - $b) / $delta;
-        if ($max == $g && $max != $b) $h += (2 + ($b - $r) / $delta);
-        if ($max == $b && $max != $r) $h += (4 + ($r - $g) / $delta);
+        if ($max == $r && $max != $g) {
+            $h += ($g - $b) / $delta;
+        }
+        if ($max == $g && $max != $b) {
+            $h += (2 + ($b - $r) / $delta);
+        }
+        if ($max == $b && $max != $r) {
+            $h += (4 + ($r - $g) / $delta);
+        }
         $h /= 6;
     }
     return array($h, $s, $l);
@@ -192,21 +198,30 @@ function _color_hsl2rgb($hsl) {
 ### Helper function for _color_hsl2rgb().
 function _color_hue2rgb($m1, $m2, $h) {
     $h = ($h < 0) ? $h + 1 : (($h > 1) ? $h - 1 : $h);
-    if ($h * 6 < 1) return $m1 + ($m2 - $m1) * $h * 6;
-    if ($h * 2 < 1) return $m2;
-    if ($h * 3 < 2) return $m1 + ($m2 - $m1) * (0.66666 - $h) * 6;
+    if ($h * 6 < 1) {
+        return $m1 + ($m2 - $m1) * $h * 6;
+    }
+    if ($h * 2 < 1) {
+        return $m2;
+    }
+    if ($h * 3 < 2) {
+        return $m1 + ($m2 - $m1) * (0.66666 - $h) * 6;
+    }
     return $m1;
 }
 
 ### Convert a hex color into an RGB triplet.
 function _color_unpack($hex, $normalize = false) {
 
-    if (strpos($hex, '#') !== 0)
+    if (strpos($hex, '#') !== 0) {
         $hex = _resolve_htmlcolor($hex);
+    }
 
     if (strlen($hex) == 4) {
         $hex = $hex[1] . $hex[1] . $hex[2] . $hex[2] . $hex[3] . $hex[3];
-    } $c = hexdec($hex);
+    }
+    $c = hexdec($hex);
+
     for ($i = 16; $i >= 0; $i -= 8) {
         $out[] = (($c >> $i) & 0xFF) / ($normalize ? 255 : 1);
     }
@@ -246,12 +261,19 @@ function rgb2hsl($arr) {
         $del_G = ((($var_Max - $var_G ) / 6 ) + ($del_Max / 2 ) ) / $del_Max;
         $del_B = ((($var_Max - $var_B ) / 6 ) + ($del_Max / 2 ) ) / $del_Max;
 
-        if      ($var_R == $var_Max) $h = $del_B - $del_G;
-        else if ($var_G == $var_Max) $h = (1 / 3 ) + $del_R - $del_B;
-        else if ($var_B == $var_Max) $h = (2 / 3 ) + $del_G - $del_R;
+        if ($var_R == $var_Max) {
+            $h = $del_B - $del_G;
+        } elseif ($var_G == $var_Max) {
+            $h = (1 / 3 ) + $del_R - $del_B;
+        } elseif ($var_B == $var_Max) {
+            $h = (2 / 3 ) + $del_G - $del_R;
+        }
 
-        if ($h < 0) $h++;
-        if ($h > 1) $h--;
+        if ($h < 0) {
+            $h++;
+        } elseif ($h > 1) {
+            $h--;
+        }
    }
 
    return array($h, $s, $v);
@@ -262,7 +284,7 @@ function hsl2rgb($arr) {
     $s = $arr[1];
     $v = $arr[2];
 
-    if($s == 0) {
+    if ($s == 0) {
         $r = $g = $B = $v * 255;
     } else {
         $var_H = $h * 6;
@@ -271,12 +293,31 @@ function hsl2rgb($arr) {
         $var_2 = $v * (1 - $s * ($var_H - $var_i ) );
         $var_3 = $v * (1 - $s * (1 - ($var_H - $var_i ) ) );
 
-        if       ($var_i == 0) { $var_R = $v     ; $var_G = $var_3  ; $var_B = $var_1 ; }
-        else if  ($var_i == 1) { $var_R = $var_2 ; $var_G = $v      ; $var_B = $var_1 ; }
-        else if  ($var_i == 2) { $var_R = $var_1 ; $var_G = $v      ; $var_B = $var_3 ; }
-        else if  ($var_i == 3) { $var_R = $var_1 ; $var_G = $var_2  ; $var_B = $v     ; }
-        else if  ($var_i == 4) { $var_R = $var_3 ; $var_G = $var_1  ; $var_B = $v     ; }
-        else                   { $var_R = $v     ; $var_G = $var_1  ; $var_B = $var_2 ; }
+        if ($var_i == 0) {
+            $var_R = $v;
+            $var_G = $var_3;
+            $var_B = $var_1;
+        } elseif ($var_i == 1) {
+            $var_R = $var_2;
+            $var_G = $v;
+            $var_B = $var_1;
+        } elseif ($var_i == 2) {
+            $var_R = $var_1;
+            $var_G = $v;
+            $var_B = $var_3 ;
+        } elseif ($var_i == 3) {
+            $var_R = $var_1;
+            $var_G = $var_2;
+            $var_B = $v;
+        } elseif ($var_i == 4) {
+            $var_R = $var_3;
+            $var_G = $var_1;
+            $var_B = $v;
+        } else {
+            $var_R = $v;
+            $var_G = $var_1;
+            $var_B = $var_2 ;
+        }
 
         $r = $var_R * 255;
         $g = $var_G * 255;
@@ -295,36 +336,37 @@ function colorPalette($imageFile, $numColors, $granularity = 5) {
     if (strtolower($size['mime']) == 'image/vnd.microsoft.icon') {
 
         if (class_exists("floIcon")) {
-
             $ico = new floIcon();
             @$ico->readICO($imageFile);
 
-            if(count($ico->images)==0)
+            if (count($ico->images) == 0) {
                 return false;
-            else
-                $img = @$ico->images[count($ico->images)-1]->getImageResource();
+            }
+            $img = @$ico->images[count($ico->images)-1]->getImageResource();
 
         } else {
             return false;
         }
 
-    } else if ($size[0] > 0 && $size[1] > 0) {
+    } elseif ($size[0] > 0 && $size[1] > 0) {
         $img = @imagecreatefromstring(file_get_contents($imageFile));
     }
 
-    if (!$img) return false;
+    if (!$img) {
+        return false;
+    }
 
-    for($x = 0; $x < $size[0]; $x += $granularity) {
-        for($y = 0; $y < $size[1]; $y += $granularity) {
+    for ($x = 0; $x < $size[0]; $x += $granularity) {
+        for ($y = 0; $y < $size[1]; $y += $granularity) {
             $thisColor = imagecolorat($img, $x, $y);
             $rgb = imagecolorsforindex($img, $thisColor);
             $red = round(round(($rgb['red'] / 0x33)) * 0x33);
             $green = round(round(($rgb['green'] / 0x33)) * 0x33);
             $blue = round(round(($rgb['blue'] / 0x33)) * 0x33);
             $thisRGB = sprintf('%02X%02X%02X', $red, $green, $blue);
-            if(array_key_exists($thisRGB, $colors)) {
+            if (array_key_exists($thisRGB, $colors)) {
                 $colors[$thisRGB]++;
-            } else{
+            } else {
                 $colors[$thisRGB] = 1;
             }
         }
