e23590d2766ce1585c4261a59679b521fb94a5da
Michael Käufl <big-fat-rss@c.michael-kaeufl.de>
2014-11-19 00:36:30 +0100

    Fixed issues reported by phpcs

--- include/crypt.php
+++ include/crypt.php
@@ -1,5 +1,6 @@
 <?php
-function decrypt_string($str) {
+function decrypt_string($str)
+{
     $pair = explode(":", $str);
 
     if (count($pair) == 2) {
@@ -9,24 +10,39 @@ function decrypt_string($str) {
         if ($iv && $encstr) {
             $key = hash('SHA256', FEED_CRYPT_KEY, true);
 
-            $str = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $encstr,
-                MCRYPT_MODE_CBC, $iv);
+            $str = mcrypt_decrypt(
+                MCRYPT_RIJNDAEL_128,
+                $key,
+                $encstr,
+                MCRYPT_MODE_CBC,
+                $iv
+            );
 
-            if ($str) return rtrim($str);
+            if ($str) {
+                return rtrim($str);
+            }
         }
     }
 
     return false;
 }
 
-function encrypt_string($str) {
+function encrypt_string($str)
+{
     $key = hash('SHA256', FEED_CRYPT_KEY, true);
 
-    $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128,
-        MCRYPT_MODE_CBC), MCRYPT_RAND);
+    $iv = mcrypt_create_iv(
+        mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC),
+        MCRYPT_RAND
+    );
 
-    $encstr = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $str,
-        MCRYPT_MODE_CBC, $iv);
+    $encstr = mcrypt_encrypt(
+        MCRYPT_RIJNDAEL_128,
+        $key,
+        $str,
+        MCRYPT_MODE_CBC,
+        $iv
+    );
 
     $iv_base64 = base64_encode($iv);
     $encstr_base64 = base64_encode($encstr);
