1b1139f2fbe52e6ed4a667f4def3d4a287f4748a
Michael Käufl <big-fat-rss@c.michael-kaeufl.de>
2014-11-19 23:08:16 +0100

    Removed unnecessary local variable

--- image.php
+++ image.php
@@ -29,16 +29,13 @@ if ($hash) {
 
     if (file_exists($filename)) {
         /* See if we can use X-Sendfile */
-        $xsendfile = false;
         if (function_exists('apache_get_modules') &&
             array_search('mod_xsendfile', apache_get_modules())) {
-            $xsendfile = true;
-        }
 
-        if ($xsendfile) {
             header("X-Sendfile: $filename");
             header("Content-type: application/octet-stream");
             header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+
         } else {
             header("Content-type: image/png");
             $stamp = gmdate("D, d M Y H:i:s", filemtime($filename)). " GMT";
