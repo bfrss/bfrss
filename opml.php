0ff84f65c40c4fdd81700aacef8d4e9509b75e6f
Michael Käufl <big-fat-rss@c.michael-kaeufl.de>
2014-11-19 23:12:08 +0100

    Fixed issues reported by phpcs

--- opml.php
+++ opml.php
@@ -1,6 +1,7 @@
 <?php
-set_include_path(dirname(__FILE__) ."/include" . PATH_SEPARATOR .
-    get_include_path());
+set_include_path(
+    dirname(__FILE__) ."/include" . PATH_SEPARATOR . get_include_path()
+);
 
 require_once "autoload.php";
 require_once "functions.php";
@@ -10,16 +11,20 @@ require_once "config.php";
 require_once "db.php";
 require_once "db-prefs.php";
 
-if (!init_plugins()) return;
+if (!init_plugins()) {
+    return;
+}
 
 $op = $_REQUEST['op'];
 
-if ($op == "publish"){
-    $key = db_escape_string( $_REQUEST["key"]);
+if ($op == "publish") {
+    $key = db_escape_string($_REQUEST["key"]);
 
-    $result = db_query( "SELECT owner_uid
+    $result = db_query(
+        "SELECT owner_uid
         FROM ttrss_access_keys WHERE
-        access_key = '$key' AND feed_id = 'OPML:Publish'");
+        access_key = '$key' AND feed_id = 'OPML:Publish'"
+    );
 
     if (db_num_rows($result) == 1) {
         $owner_uid = db_fetch_result($result, 0, "owner_uid");
