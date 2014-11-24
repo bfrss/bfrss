6a88b0ac3f18c1679f56bd503080a6978a589861
Michael Käufl <big-fat-rss@c.michael-kaeufl.de>
2014-11-24 01:01:46 +0100

    Tabs to spaces, closing tags, line endings

--- classes/handler.php
+++ classes/handler.php
@@ -1,24 +1,22 @@
 <?php
 class Handler implements IHandler {
-	protected $dbh;
-	protected $args;
+    protected $dbh;
+    protected $args;
 
-	function __construct($args) {
-		$this->dbh = Db::get();
-		$this->args = $args;
-	}
+    function __construct($args) {
+        $this->dbh = Db::get();
+        $this->args = $args;
+    }
 
-	function csrf_ignore($method) {
-		return true;
-	}
+    function csrf_ignore($method) {
+        return true;
+    }
 
-	function before($method) {
-		return true;
-	}
+    function before($method) {
+        return true;
+    }
 
-	function after() {
-		return true;
-	}
-
+    function after() {
+        return true;
+    }
 }
-?>
